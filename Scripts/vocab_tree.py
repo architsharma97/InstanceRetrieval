4489959ddd4a44a0c765347391553edad32168d0
Kanishk Gandhi <kanishk@artifacia.com>
2017-01-29 19:57:56 +0530

    add heirarchical clustering

---
+++ Scripts/vocab_tree.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+visual_words = np.load('visual_words.npy')
+
+print("Compute unstructured hierarchical clustering...")
+st = time.time()
+ward = AgglomerativeClustering(n_clusters=6, linkage='ward').fit(visual_words)
+elapsed_time = time.time() - st
+label = ward.labels_
+print("Elapsed time: %.2fs" % elapsed_time)
+print("Number of points: %i" % label.size)
+

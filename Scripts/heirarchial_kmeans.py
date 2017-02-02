9379497581a5888af620b211f345bc26cf1bea72
Kanishk Gandhi <kanishk@artifacia.com>
2017-02-02 18:58:09 +0530

    modify hierarchical K-Means

--- Scripts/heirarchial_kmeans.py
+++ Scripts/heirarchial_kmeans.py
@@ -4,26 +4,30 @@ import math
 
 class h_kmeans:
 
-	def __init__(data_matrix, num_leaves = None):
+	def __init__(self, data_matrix = None, num_leaves = None):
+
 		self.data_matrix = data_matrix
 		self.num_leaves = num_leaves
 		self.tree = None
 
-	def cluster(branch_factor = 3):
+	def cluster(self, branch_factor = 3):
 		self.tree = []
-		labels = []
+		self.labels = []
 		self.branch_factor = branch_factor
+
 		num_data_points = self.data_matrix.shape[0]
-		num_layers = math.log(num_data_points, self.branch_factor)
+		num_layers = int(math.log(num_data_points, self.branch_factor))
 		num_layer_cluster = num_data_points/self.branch_factor #convert to ceil
 		X = self.data_matrix
 		self.tree.append(X)
+
 		for i in range(num_layers):
+			print(num_layers)
+			print(num_layer_cluster)
+
 			kmeans = KMeans(num_layer_cluster, random_state=0).fit(X)
-			labels.append(kmeans.labels_)
+			self.labels.append(kmeans.labels_)
+			print(kmeans.labels_)
+
 			X = kmeans.cluster_centers_
 			num_layer_cluster = num_layer_cluster/self.branch_factor
-
-
-
-

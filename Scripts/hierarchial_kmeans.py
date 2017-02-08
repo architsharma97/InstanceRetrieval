d29a2c2f5e1920d0f82153601836f13d307ca9e4
Kanishk Gandhi <kanishk@artifacia.com>
2017-02-08 17:40:59 +0530

    add query processing

--- Scripts/hierarchial_kmeans.py
+++ Scripts/hierarchial_kmeans.py
@@ -2,9 +2,25 @@ import numpy as np
 from sklearn.cluster import KMeans
 import math
 from collections import Counter
-import pickle
+import pkl_utils
 
+def feature_predict(kmeans_tree,feature,num_layer = 1, cluster_inf = '0'):
+		distance = []
+		feature = feature/ np.linalg.norm(feature)
+		end = False
+		for i in range(kmeans_tree.branch_factor):
+			if (str(num_layer)+';'+str(cluster_inf)+str(i)) in kmeans_tree.tree_info.keys():
+				distance.append(np.dot(np.transpose( kmeans_tree.tree_info[str(num_layer)+';'+str(cluster_inf)+str(i)]),feature))
+			else:
+				print("else "+str(num_layer)+';'+str(cluster_inf)+str(i))
+				distance.append(np.dot(np.transpose( kmeans_tree.tree_info[str(num_layer)+';'+str(cluster_inf)+str(i)+'end']),feature))
+				end = True
+		if str(num_layer)+';'+str(cluster_inf)+str(distance.index(max(distance)))+'end' in kmeans_tree.tree_info.keys():
+			indices =  kmeans_tree.branch_info[str(num_layer)+';'+str(cluster_inf)+str(distance.index(max(distance)))+'indices'+'end'+'image']
+			weight =  kmeans_tree.branch_info[str(num_layer)+';'+str(cluster_inf)+str(distance.index(max(distance)))+'indices'+'end'+'weights']
+			return indices,weight
 
+		return feature_predict(kmeans_tree, feature, num_layer+1, cluster_inf+str(distance.index(max(distance))))
 
 class h_kmeans:
 	"""
@@ -60,7 +76,7 @@ class h_kmeans:
 			labels = kmeans.labels_
 			
 			for i in range(num_cluster):
-				indices = np.array([j for /j, x in enumerate(labels) if x == i])
+				indices = np.array([j for j, x in enumerate(labels) if x == i])
 				
 				new_cluster = data_cluster[indices,:]
 
@@ -77,6 +93,7 @@ class h_kmeans:
 
 		sub_cluster(self.data_matrix,self.branch_factor,0, '0')
 
+
 		for key in self.branch_info.keys():
 			if 'end' in key:
 				node_indices = self.branch_info[key]
@@ -85,29 +102,17 @@ class h_kmeans:
 				self.branch_info[key+'image'] = image_indices
 				self.branch_info[key+'weights'] = math.log(float(self.num_files)/ len(image_indices))
 
+		print("She's gonna blow")
+		pkl_utils.save_obj(self,'../Models/porcupine.pkl')
+		print("Bomb Defused")
 
 
-	def feature_predict(kmeans_tree,feature,layer = 0, cluster_inf = '0'):
-		distance = []
-		feature = feature/ np.linalg.norm(feature)
-		end = False
-		for i in range(kmeans_tree.branch_factor):
-			if str(num_layer)+';'+str(cluster_inf)+str(i) in kmeans_tree.tree_info.keys:
-				distance.append(np.dot(np.transpose( kmeans_tree.tree_info[str(num_layer)+';'+str(cluster_inf)+str(i)]),feature))
-			else:
-				distance.append(np.dot(np.transpose( kmeans_tree.tree_info[str(num_layer)+';'+str(cluster_inf)+str(i)+'end']),feature))
-				end = True
-		if end:
-			indices =  kmeans_tree.branch_info[str(num_layer)+';'+str(cluster_inf)+str(distance.index(max(distance)))+'indices+'+'end'+'image']
-			weight =  kmeans_tree.branch_info[str(num_layer)+';'+str(cluster_inf)+str(distance.index(max(distance)))+'indices+'+'end'+'weights']
-			return indices,weight
-
-		return predict(kmeans_tree, feature, layer+1, cluster_inf+str(distance.index(max(distance))))
+	
 
 	def query(kmeans_tree,feature_matrix):
 		scores = np.zeros(kmeans_tree.num_files)
 		for feature in feature_matrix:
-			indices, weight = feature_predict(feature)
+			indices, weight = feature_predict(kmeans_tree, feature)
 			for index in indices:
 				scores[index]+=weight
 		return scores

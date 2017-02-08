429397e9a9133bb8d32f3f06947761211a59e531
architsharma97 <archit97sharma@gmail.com>
2017-02-08 13:17:22 +0530

    Modified for Validation

--- Scripts/aux_vocab_reduction.py
+++ Scripts/aux_vocab_reduction.py
@@ -2,21 +2,28 @@ import sys
 from pkl_utils import save_obj, load_obj
 import numpy as np
 
-# Argument 1: Reduced dimesnsions
+# Argument 1: Reduced dimensions
+# Argument 2: Train (0) or Val(1)
 dim_red=int(sys.argv[1])
+sys2=int(sys.argv[2])
 
-matrix_sizes=[long(size) for size in open('../Models/VW_Train/shape_info.txt','r').read().splitlines()]
-
-# final reduced matrix
-visual_words_reduced=np.zeros((matrix_sizes[8],dim_red))
-
+if not sys2:
+	matrix_sizes=[long(size) for size in open('../Models/VW_Train/shape_info.txt','r').read().splitlines()]
+	# final reduced matrix
+	visual_words_reduced=np.zeros((matrix_sizes[8],dim_red))
+	num_files=range(1,9)
+else:
+	matrix_sizes=[long(size) for size in open('../Models/VW_Val/shape_info.txt','r').read().splitlines()]
+	# final reduced matrix
+	visual_words_reduced=np.zeros((matrix_sizes[0],dim_red))]
+	num_files=range(1,2)
 # loading precomputed pca
 pca=load_obj('../Models/PCAlayer_128.pkl')
 
 done_till_now=0
 
-# load 2nd, 3rd, 4th, 5th, 6th, 7th and 8th matrix separately and reduce them
-for i in range(1,9):
+# load matrices separately and reduce them
+for i in num_files:
 	print "Loading visual_words_" + str(i)
 	visual_words=np.load('../Models/VW_Train/visual_words_'+str(i)+'.npy')
 	visual_words_reduced[done_till_now:done_till_now+matrix_sizes[i-1],:]=pca.transform(visual_words)

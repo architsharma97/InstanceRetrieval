bdbf87f611899471971e9b269a79a0be006879a5
architsharma97 <archit97sharma@gmail.com>
2017-02-09 16:46:34 +0530

    Fixed bugs in query

--- Scripts/query.py
+++ Scripts/query.py
@@ -54,7 +54,7 @@ feature_matrix=model.predict(processed_regions)
 feature_matrix=pca.transform(feature_matrix)
 
 print ("Getting the rankings")
-score=vocab_tree.query_tree(vocab_tree, feature_matrix)
+score=vocab_tree.query(vocab_tree, feature_matrix)
 rankings=sorted(range(len(score)), key=lambda k: score[k])
 
 train_file_names=open(sys.argv[4],'r').read().splitlines()

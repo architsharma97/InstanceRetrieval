1a315f19b46690ecd7f2ba0f0fda862ec851133f
architsharma97 <archit97sharma@gmail.com>
2017-02-10 18:54:05 +0530

    Changed PCA files

--- Scripts/feature_reduction.py
+++ Scripts/feature_reduction.py
@@ -23,7 +23,7 @@ for i in range(1):
 feature_matrix=np.zeros((num_words, 4096))
 
 # final reduced matrix
-visual_words_reduced=np.zeros((matrix_sizes[8],dim_red))
+# visual_words_reduced=np.zeros((matrix_sizes[8],dim_red))
 
 done_till_now=0
 t1=time.time()
@@ -36,29 +36,33 @@ t2=time.time()
 print "Performing Dimensionality reduction"
 pca=PCA(n_components=dim_red)
 
-print "Reducing and copying the initial 1 matrices"
-visual_words_reduced[:feature_matrix.shape[0],:]=pca.fit_transform(feature_matrix)
+print "Obtaining PCA Matrix"
+pca.fit(feature_matrix)
 
 t3=time.time()
 print "Saving PCA matrix in a pickle object"
 save_obj(pca, '../Models/PCAlayer_'+str(dim_red)+'.pkl')
 
 t4=time.time()
-print "Loading the rest of matrices and reducing them"
 
-# load 2nd, 3rd, 4th, 5th, 6th, 7th and 8th matrix separately and reduce them
-for i in range(2,9):
-	visual_words=np.load('../Models/VW_Train/visual_words_'+str(i)+'.npy')
-	visual_words_reduced[done_till_now:done_till_now+matrix_sizes[i-1],:]=pca.transform(visual_words)
-	done_till_now+=matrix_sizes[i-1]
+# Shifted to another python file
+# print "Loading the rest of matrices and reducing them"
 
-t5=time.time()
-print "Saving the complete reduced matrix"
-np.save('../Models/VW_Train/visual_words_reduced.npy',visual_words_reduced)
-t6=time.time()
+# # load 2nd, 3rd, 4th, 5th, 6th, 7th and 8th matrix separately and reduce them
+# for i in range(2,9):
+# 	visual_words=np.load('../Models/VW_Train/visual_words_'+str(i)+'.npy')
+# 	visual_words_reduced[done_till_now:done_till_now+matrix_sizes[i-1],:]=pca.transform(visual_words)
+# 	done_till_now+=matrix_sizes[i-1]
+
+# t5=time.time()
+# print "Saving the complete reduced matrix"
+# np.save('../Models/VW_Train/visual_words_reduced.npy',visual_words_reduced)
+# t6=time.time()
 
 print "Time to load first 1 visual words matrices: " + str(t2-t1)
 print "Time to perform PCA: " + str(t3-t2)
 print "Time to save the PCA object: " + str(t4-t3)
-print "Time to load and reduce the rest of matrices: " + str(t5-t4)
-print "Time to save the complete reduced matrix: " + str(t6-t5)
+
+# Shifted to another python file
+# print "Time to load and reduce the rest of matrices: " + str(t5-t4)
+# print "Time to save the complete reduced matrix: " + str(t6-t5)

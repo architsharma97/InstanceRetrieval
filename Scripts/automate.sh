22d781f45df97f8382110061c375fd2c69f677c9
architsharma97 <archit97sharma@gmail.com>
2017-02-11 22:46:51 +0530

    Added script to automate training and validation

---
+++ Scripts/automate.sh
@@ -0,0 +1,4 @@
+python train_val.py 0 10 7
+python train_val.py 1 ../Models/vocab_tree_10_7/vocab_tree_10_7.pkl ../Models/vocab_tree_10_7/results_val.txt
+python train_val.py 0 100 3
+python train_val.py 1 ../Models/vocab_tree_100_3/vocab_tree_100_3.pkl ../Models/vocab_tree_100_7/results_val.txt
